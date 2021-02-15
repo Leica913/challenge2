11f9dcfed866bd747fe06c26833ffc6f8e48ead4
Leica913 <toruneko1996913@gmail.com>
2021-02-15 08:59:21 +0000

    [Finish,Update] 4-8

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -7,47 +7,44 @@ class BooksController < ApplicationController
 
 
   def edit
-  	@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
 
   def show
-  	@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
 
   def create
     @books = Book.all
     @book = Book.new(book_params)
-      if @book.save
-        flash[:notice] = "successfully!"
-        redirect_to book_path(@book)
-      else
-        flash[:notice] = "error!"
-        flash.now[:error] = @book.errors.full_messages
-        render :index
-      end
+    if @book.save
+      flash[:notice] = "successfully!"
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "successfully!"
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
-      flash[:notice] = "error"
-      flash.now[:error] = @book.errors.full_messages
       render :edit
     end
   end
 
 
   def destroy
-  	book = Book.find(params[:id])
-  	book.destroy
-  	flash[:notice] = "successfully!"
-  	redirect_to books_path
+    @book = Book.find(params[:id])
+    if @book.destroy
+     flash[:notice] = "successfully!"
+     redirect_to books_path
+    end
   end
 
 
